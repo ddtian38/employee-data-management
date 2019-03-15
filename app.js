4cf232f74a79304cd84747e4764b366346499b23
Daniel Tian <danieltian31@yahoo.com>
2019-03-15 12:02:27 -0500

    js file with DB credentials

---
+++ app.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCMgSS7i2CBJE5BdmaIah7gZEk6DmTLglQ",
+    authDomain: "employee-data-management-61f3a.firebaseapp.com",
+    databaseURL: "https://employee-data-management-61f3a.firebaseio.com",
+    projectId: "employee-data-management-61f3a",
+    storageBucket: "employee-data-management-61f3a.appspot.com",
+    messagingSenderId: "605542098125"
+  };
+  firebase.initializeApp(config);
+</script>
